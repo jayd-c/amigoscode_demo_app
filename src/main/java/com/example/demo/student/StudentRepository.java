76ce11aa4e0739efe19bf58bb610c5837dc7b989
JayanDesilva <jaycdesilva@gmail.com>
2024-09-18 11:39:15 -0400

    added ropository and config to store students manually

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,2 +1,10 @@
-package com.example.demo.student;public interface StudentRepository {
+package com.example.demo.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long>{
+
 }
