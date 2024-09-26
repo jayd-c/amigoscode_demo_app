36664bc48b59f2d0289b433c06dd6a4142969023
JayanDesilva <jaycdesilva@gmail.com>
2024-09-25 22:01:01 -0400

    configuration file modified

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -13,16 +15,14 @@ public class StudentConfig {
 
     @Bean
     CommandLineRunner commandLineRunner (StudentRepository repository) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Mariam","Mariam@gmail.com", LocalDate.of(2001, Month.JANUARY,5)));
+        students.add(new Student("Alex","Alex@gmail.com", LocalDate.of(1979, Month.MARCH,25)));
+        students.add(new Student("Brian","brian@gmail.com", LocalDate.of(1984, Month.JANUARY,5)));
         return args -> {
-           Student mariam = new Student(
-                    "Mariam","Mariam@gmail.com", LocalDate.of(2000, Month.JANUARY,5)
-            );
-            Student alex = new Student(
-                 "Alex","Alex@gmail.com", LocalDate.of(2000, Month.JANUARY,5)
-            );
-            repository.saveAll(
-                    List.of(mariam,alex)
-            );
+
+            repository.saveAll(students);
+            System.out.println(Period.between(LocalDate.ofEpochDay(LocalDate.now().getYear()),LocalDate.of(1979,03,25)));
         };
     }
 }
